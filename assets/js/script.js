da4a0b997822a73e49c26a30530ec6cd4ab0fe43
PaulHeruko <whatthesheesh1@protonmail.com>
2025-10-05 13:24:04 +0200

    moved nav bar to navbar.html

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,3 +18,20 @@ fetch("../assets/html/footer.html")
     document.getElementById("footer").innerHTML = data;
   });
 
+fetch("../assets/html/navbar.html")
+  .then(response => response.text())
+  .then(data => {
+    document.getElementById("navbar").innerHTML = data;
+  });
+
+document.addEventListener("DOMContentLoaded", () => {
+  const navbar = document.querySelector(".navbar");
+
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > 0) {
+      navbar.classList.add("scrolled");
+    } else {
+      navbar.classList.remove("scrolled");
+    }
+  });
+});
